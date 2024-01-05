55117d428006f63aca792caea6a363e562365f48
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2024-01-05 12:16:37 +0530

    new script for calculate time

--- bug_tracking/bug_tracking/doctype/bugs_or_issue/bugs_or_issue.js
+++ bug_tracking/bug_tracking/doctype/bugs_or_issue/bugs_or_issue.js
@@ -1,8 +1,37 @@
 // Copyright (c) 2024, Sanskar Technolab and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Bugs or Issue", {
-// 	refresh(frm) {
+frappe.ui.form.on('Bugs or Issue', {
+    refresh: function(frm) {
+        // Trigger the calculation when the form is loaded
+        calculateTimeDifference(frm);
+    },
+    
+    reported_on: function(frm) {
+        // Trigger the calculation when the start_datetime field changes
+        calculateTimeDifference(frm);
+    },
+    
+    resolved_on: function(frm) {
+        // Trigger the calculation when the end_datetime field changes
+        calculateTimeDifference(frm);
+    }
+});
 
-// 	},
-// });
+function calculateTimeDifference(frm) {
+    var startDatetime = frm.doc.reported_on;
+    var endDatetime = frm.doc.resolved_on;
+
+    if (startDatetime && endDatetime) {
+        // Convert date strings to Date objects
+        var startDate = new Date(startDatetime);
+        var endDate = new Date(endDatetime);
+
+        // Calculate the time difference in milliseconds
+        var timeDifference = endDate - startDate;
+
+        // Convert milliseconds to seconds
+        var secondsDifference = timeDifference / 1000;
+        frm.set_value('resolved_time', secondsDifference);
+    }
+}

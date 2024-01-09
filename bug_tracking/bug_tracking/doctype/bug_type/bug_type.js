084f7640b0edb1e2da2092db8ad4e29d2217812f
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2024-01-09 16:55:53 +0530

    new bug type field in issue to filter client and internal bug

--- bug_tracking/bug_tracking/doctype/bug_type/bug_type.js
+++ bug_tracking/bug_tracking/doctype/bug_type/bug_type.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Sanskar Technolab and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("test", {
+// frappe.ui.form.on("Bug Type", {
 // 	refresh(frm) {
 
 // 	},
